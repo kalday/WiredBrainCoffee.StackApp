3de33d85db1b4ced4bd911d26329920cdd60263e
BARAJA\kyra.alday <kyra.alday@baraja.com>
2022-03-03 16:53:08 +1100

    2 - create a generic stack class

--- WiredBrainCoffee.StackApp/Program.cs
+++ WiredBrainCoffee.StackApp/Program.cs
@@ -6,11 +6,43 @@ namespace WiredBrainCoffee.StackApp
     {
         static void Main(string[] args)
         {
-            var stack = new SimpleStack();
+            StackDoubles();
+            StackStrings();
+            Console.ReadLine();
+        }
+
+        private static void StackDoubles()
+        {
+            var stack = new SimpleStackDouble();
 
             stack.Push(1.2);
             stack.Push(2.8);
             stack.Push(3.0);
+
+            double sum = 0.0;
+
+            while (stack.Count > 0)
+            {
+                double item = stack.Pop();
+                Console.WriteLine($"Item: {item}");
+                sum += item;
+            }
+
+            Console.WriteLine($"Sum: {sum}");
         }
+
+        private static void StackStrings()
+        {
+            var stack = new SimpleStackString();
+
+            stack.Push("Wired Brain Coffee");
+            stack.Push("Pluralsight");
+
+            while (stack.Count > 0)
+            {
+                Console.WriteLine(stack.Pop());
+            }
+        }
+
     }
 }
--- WiredBrainCoffee.StackApp/SimpleStack.cs
+++ WiredBrainCoffee.StackApp/SimpleStack.cs
@@ -1,14 +1,28 @@
 ﻿namespace WiredBrainCoffee.StackApp
 {
-    internal class SimpleStack
+    public class SimpleStackDouble
     {
-        public SimpleStack()
-        {
-        }
+        private readonly double[] _items;
+        private int _currentIndex = -1;
 
-        internal void Push(double v)
-        {
-            throw new NotImplementedException();
-        }
+        public SimpleStackDouble() => _items = new double[10]; 
+        public int Count => _currentIndex + 1;
+
+        public void Push(double item) => _items[++_currentIndex] = item;
+    
+        public double Pop() => _items[_currentIndex--];
+    }
+
+    public class SimpleStackString
+    {
+        private readonly string[] _items;
+        private int _currentIndex = -1;
+
+        public SimpleStackString() => _items = new string[10];
+        public int Count => _currentIndex + 1;
+
+        public void Push(string item) => _items[++_currentIndex] = item;
+
+        public string Pop() => _items[_currentIndex--];
     }
 }
