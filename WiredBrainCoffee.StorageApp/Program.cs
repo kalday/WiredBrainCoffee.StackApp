51dcb69161b96d30f538b40fe25eb342452837cc
BARAJA\kyra.alday <kyra.alday@baraja.com>
2022-03-04 11:58:26 +1100

    3 -- implementing generic classes

---
+++ WiredBrainCoffee.StorageApp/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using WiredBrainCoffee.StorageApp.Entities;
+using WiredBrainCoffee.StorageApp.Repositories;
+
+namespace WiredBrainCoffee.StorageApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var employeeRepository = new GenericRepository<Employee>();
+            AddEmployees(employeeRepository);
+            GetEmployeeById(employeeRepository);
+
+            var organisationRepository = new GenericRepository<Organisation>();
+            AddOrganisations(organisationRepository);
+
+            Console.ReadLine();
+
+        }
+
+        private static void GetEmployeeById(GenericRepository<Employee> employeeRepository)
+        {
+            var employee = employeeRepository.GetById(2);
+            Console.WriteLine($"Employee with ID 2: {employee.FirstName}");
+        }
+
+        private static void AddEmployees(GenericRepository<Employee> employeeRepository)
+        {
+            employeeRepository.Add(new Employee { FirstName = "Julia" });
+            employeeRepository.Add(new Employee { FirstName = "Anna" });
+            employeeRepository.Add(new Employee { FirstName = "Thomas" });
+            employeeRepository.Save();
+        }
+
+        private static void AddOrganisations(GenericRepository<Organisation> organisationRepository)
+        {
+            organisationRepository.Add(new Organisation { Name = "Pluralsight" });
+            organisationRepository.Add(new Organisation { Name = "Globomantics" });
+            organisationRepository.Save();
+        }
+    }
+}
